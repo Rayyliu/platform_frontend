9031a5f4c78d68b527a5286994d136f754adcf55
lr <466088269@qq.com>
2020-08-19 22:40:46 +0800

    编辑body页面

--- src/pages/Transfer/EditBodyTabs.js
+++ src/pages/Transfer/EditBodyTabs.js
@@ -1,16 +1,21 @@
-import EditableCell from "./EditableCell";
-import {Button, Form,Table,Popconfirm,Select,Tabs} from "antd";
+import {Button, Form, Table, Popconfirm, Select, Tabs, Input} from "antd";
 import React from "react";
 
+const initialPanes = [
+    { key: "1" }
+];
 
-
+const {TextArea} = Input
+const { TabPane } = Tabs;
 class EditBodyTabs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            panes:[]
-        }
-    }
+    newTabIndex = 0;
+    count=1;
+
+    state = {
+        activeKey: initialPanes[0].key,
+        panes: initialPanes,
+
+    };
 
 
     componentDidMount() {
@@ -19,80 +24,77 @@ class EditBodyTabs extends React.Component {
         }
     }
 
-    handleDelete = (key) => {
-        const dataSource = [...this.state.dataSource];
-        this.setState({
-            dataSource: dataSource.filter((item) => item.key !== key)
-        });
-    };
-
-    handleAdd = () => {
-        console.log("进入add方法");
-        const { count, dataSource } = this.state;
-        const newData = {
-            key: count,
-            // name: `Edward King ${count}`,
-            parameter: '',
-            except: '',
-            rule:this.columns.rule,
-        };
-        console.log("Edit---newData==="+JSON.stringify(newData))
-        this.setState({
-            dataSource: [...dataSource, newData],
-            count: count + 1
-        });
-    };
-
-    handleSave = (row) => {
-        const newData = [...this.state.dataSource];
-        const index = newData.findIndex((item) => row.key === item.key);
-        const item = newData[index];
-        newData.splice(index, 1, {
-            ...item,
-            ...row
-        });
-        console.log("newData=="+JSON.stringify(newData))
-        this.setState({ dataSource: newData });
-    };
-
-
-
-    render() {
+    //新增TabPane标签页
+    add = () => {
         const { panes } = this.state;
-        // const components = {
-        //     body: {
-        //         row: EditableFormRow,
-        //         cell: EditableCell
-        //     }
-        // };
-        // const columns = this.columns.map((col) => {
-        //     if (!col.editable) {
-        //         return col;
-        //     }
-        //     return {
-        //         ...col,
-        //         onCell: (record) => ({
-        //             record,
-        //             editable: col.editable,
-        //             dataIndex: col.dataIndex,
-        //             title: col.title,
-        //             handleSave: this.handleSave
-        //         })
-        //     };
-        // });
+        const activeKey = `入参${this.newTabIndex++}`;
+        console.log("activeKey==="+JSON.stringify(activeKey))
+        const newPanes = [...panes];
+        newPanes.push({
+            title: `用例${this.count++}`,
+            content: <TextArea/>,
+            key: activeKey
+        });
+        this.setState({
+            panes: newPanes,
+            activeKey
+        });
+    };
+
+    //监听
+    onChange = (activeKey) => {
+        this.setState({ activeKey });
+    };
+
+    onEdit = (targetKey, action) => {
+        this[action](targetKey);
+    };
+
+    remove = (targetKey) => {
+        const { panes, activeKey ,count} = this.state;
+        console.log("activeKey==="+JSON.stringify(activeKey))
+        console.log("targetKey==="+JSON.stringify(targetKey))
+        let newActiveKey = activeKey;
+        let lastIndex;
+        panes.forEach((pane, i) => {
+            console.log("pane==="+JSON.stringify(pane))
+            console.log("i==="+JSON.stringify(i))
+            if (pane.key === targetKey) {
+                lastIndex = i - 1;
+
+            }
+        });
+        const newPanes = panes.filter((pane) => pane.key !== targetKey);
+        if (newPanes.length && newActiveKey === targetKey) {
+            if (lastIndex >= 0) {
+                newActiveKey = newPanes[lastIndex].key;
+            } else {
+                newActiveKey = newPanes[0].key;
+            }
+        }
+        this.count=this.count-1;
+        this.setState({
+            panes: newPanes,
+            activeKey: newActiveKey
+        });
+    };
+    render() {
+        const { panes, activeKey } = this.state;
         return (
-            <div>
-                {/*<Button*/}
-                    {/*onClick={this.handleAdd}*/}
-                    {/*type="primary"*/}
-                    {/*style={{ marginBottom: 16 }}*/}
-                {/*>*/}
-                    {/*Add a row*/}
-                {/*</Button>*/}
-                {/*<Button onClick={this.props.submit}>确认</Button>*/}
-                <Tabs>
-                </Tabs>
-            </div>
+
+        <Tabs
+            type="editable-card"
+            onChange={this.onChange}
+            activeKey={activeKey}
+            onEdit={this.onEdit}
+            style={{height:'200px'}}
+        >
+            {panes.map((pane) => (
+                <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>
+                    {pane.content}
+                </TabPane>
+            ))}
+        </Tabs>
         );
     }
 }
